2c09b638beddf3bdc8ef29eaef622845292d10f3
Prateekpai <prateekvasanthpai@gmail.com>
2022-03-16 08:27:50 +0530

    Create Contact in Address Book

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace ABP
+{
+    public class Program
+    {
+        public static void Main(string[]args)
+        {
+            UC1 uc1 = new UC1();
+            uc1.Address();
+        }
+    }
+}
+    
---
+++ UC1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ABP
+{
+    public class UC1
+    {
+        public void Address()
+        {
+            string FN = "Prateek";
+            string LN = "Pai";
+            string ADDR = "Bangalore";
+            string City = "Bangalore";
+            string Mob = "9945007207";
+            string State = "Karnataka";
+            int zip = 560027;
+            string Email = "Prateekvasanthpai@gmail.com";
+            {
+                Console.WriteLine("First Name: " + FN);
+                Console.WriteLine("Last Name: " + LN);
+                Console.WriteLine("Address 1: " + ADDR);
+                Console.WriteLine("City: " + City);
+                Console.WriteLine("State: " + State);
+                Console.WriteLine("Zip Code: " + zip);
+                Console.WriteLine("Phone Number: " + Mob);
+                Console.WriteLine("E-Mail: " + Email);
+                Console.WriteLine("-------------------------------------------");
+            }
+
+
+        }
+    }
+}
